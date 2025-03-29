15c35122a776329c1c420331b72e36fef7fbcff2
EverythingHasBSOD <qasimlimurad92@gmail.com>
2025-03-29 13:28:40 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+// varibles
+let img = document.getElementById('image');
+
+// func off
+function func_off() {
+ img.src = './img/bulb_off.gif'
+}
+
+
+//func on
+function func_on() {
+ img.src = './img/bulb_on.gif'
+}
+
+
+
